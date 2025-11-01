cda35d74d329aecba30ff481f05190f3fe7b1578
Vinicius Sátiro <vinicius.satiro@cwi.com.br>
2025-11-01 14:35:35 -0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+    // canActivate: [PublicGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+];

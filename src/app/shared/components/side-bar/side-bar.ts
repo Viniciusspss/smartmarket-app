85244aad7f20d89183d8708273ca61c050ab91d6
Vinicius Sátiro <vinicius.satiro@cwi.com.br>
2025-11-01 17:49:42 -0300

    feat: add side bar component with navigation and user logout modal

---
+++ src/app/shared/components/side-bar/side-bar.ts
@@ -0,0 +1,57 @@
+import { Component, inject, input, signal } from '@angular/core';
+import { AuthService } from '../../../service/auth/auth.service';
+import { UserData } from '../../../service/customer/customer.service';
+import { Router } from '@angular/router';
+import { AsideItems } from '../aside-items/aside-items';
+import { MatDialogActions, MatDialogContent } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-side-bar',
+  imports: [AsideItems, MatInputModule, MatDialogContent, MatDialogActions],
+  templateUrl: './side-bar.html',
+})
+export class SideBar {
+  private router = inject(Router);
+  private authService = inject(AuthService);
+  public user = input.required<UserData>();
+  protected modalOpen = signal<boolean>(false);
+
+  selectButton(buttonName: 'Dashboard' | 'Products' | 'Promotions' | 'Employees' | 'Clients') {
+    if (buttonName == 'Dashboard') {
+      this.router.navigateByUrl('dashboard');
+    }
+    if (buttonName == 'Products') {
+      this.router.navigateByUrl('products');
+    }
+    if (buttonName == 'Promotions') {
+      this.router.navigateByUrl('promotions');
+    }
+    if (buttonName == 'Employees') {
+      this.router.navigateByUrl('employees');
+    }
+    if (buttonName == 'Clients') {
+      this.router.navigateByUrl('clients');
+    }
+  }
+
+  selectedButton(): string {
+    return this.router.url;
+  }
+
+  protected getInitials(name: string): string {
+    if (!name) return '';
+    const parts = name.trim().split(' ');
+    const first = parts[0]?.[0] || '';
+    const last = parts.length > 1 ? parts[parts.length - 1]?.[0] : '';
+    return (first + last).toUpperCase();
+  }
+
+  changeModal() {
+    this.modalOpen.set(!this.modalOpen());
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}

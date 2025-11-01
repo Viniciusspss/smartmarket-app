9b96c9b2247b1f0f286b1361e2ca33ce5bd83b8a
Vinicius Sátiro <vinicius.satiro@cwi.com.br>
2025-11-01 17:50:10 -0300

    feat: add dashboard component with routing and template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login,
+    // canActivate: [PublicGuard],
+  },
+  {
+    path: '',
+    component: DashboardLayout,
+    children: [{ path: 'dashboard', component: Dashboard }],
     // canActivate: [PublicGuard],
   },
   {

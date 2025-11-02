14cac227bdbd9ce1708cdf184a909a07a43c6909
Vinicius Sátiro <vinicius.satiro@cwi.com.br>
2025-11-02 17:20:21 -0300

    feat: create page for products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
+import { Products } from './pages/products/products';
 
 export const routes: Routes = [
   {
@@ -12,7 +13,10 @@ export const routes: Routes = [
   {
     path: '',
     component: DashboardLayout,
-    children: [{ path: 'dashboard', component: Dashboard }],
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      { path: 'products', component: Products },
+    ],
     // canActivate: [PublicGuard],
   },
   {
---
+++ src/app/pages/products/products.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Product } from '../../shared/models/product';
+import { products } from '../../mocks/products';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@Component({
+  selector: 'app-products',
+  imports: [MatTableModule, MatPaginatorModule, MatIconModule, MatButtonModule, MatTooltipModule],
+  templateUrl: './products.html',
+})
+export class Products {
+  displayedColumns: string[] = ['name', 'type', 'price', 'pricePromotion', 'stats', 'actions'];
+  dataSource = new MatTableDataSource<Product>(products);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  protected editProduct(productId: number) {}
+  protected deleteProduct(productId: number) {}
+}
